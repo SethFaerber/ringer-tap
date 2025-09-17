0b493c5424ce6a8e3f39e550bc598b43145d3d78
Seth Faerber <seth.faerber@ramseysolutions.com>
2025-09-17 13:52:06 -0500

    tappable anywhere

--- app/src/main/java/com/grismund/ringertap/widget/RingerWidgetProvider.kt
+++ app/src/main/java/com/grismund/ringertap/widget/RingerWidgetProvider.kt
@@ -69,15 +69,14 @@ class RingerWidgetProvider : AppWidgetProvider() {
             }
         }
 
-        // Set up click listener
+        // Set up click listener on the entire widget
         val intent = Intent(context, RingerWidgetProvider::class.java)
         intent.action = ACTION_TOGGLE_RINGER
         val pendingIntent = PendingIntent.getBroadcast(
             context, 0, intent,
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
-        views.setOnClickPendingIntent(R.id.widget_icon, pendingIntent)
-        views.setOnClickPendingIntent(R.id.widget_text, pendingIntent)
+        views.setOnClickPendingIntent(R.id.widget_root, pendingIntent)
 
         // Update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views)
